8b0f16591e1671e6634a2558b3b110f3a4fbb818
joystackinfo <joynelsoninfo@gmail.com>
2025-11-04 20:53:20 +0100

    added comment

--- server.js
+++ server.js
@@ -1,4 +1,41 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const userRoutes = require('./routes/user.routes');
+dotenv.config();
+
+
+//MIDDLEWARE
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+// ROUTES
+app.use('/api/users' , userRoutes);
+
+
+
+// Test route
+app.get('/', (req, res) => {  // create a get route
+  res.send('Hello, Welcome to the CRUD API server '); // send a response
+});
+
+
+// connect to mongodb database using mongoose
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log(' Connected to Database');
+    app.listen(process.env.PORT, () => {
+      console.log(` Server is running on port ${process.env.PORT}`);
+    });
+  }
+)
+
+
+  .catch(err => {
+    console.error(' Connection failed', err);
+  });
+
+
