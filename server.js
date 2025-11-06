ccf89e26bc022f269c1ba878095ccfd00546bd40
joystackinfo <joynelsoninfo@gmail.com>
2025-11-06 20:52:45 +0100

    tracking logs

--- server.js
+++ server.js
@@ -17,6 +17,12 @@ app.use(express.static('public'));
 app.use('/api/users' , userRoutes);
 
 
+  //FOR LOGGING TO TRACK REQUESTS
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 
 // Test route
 app.get('/', (req, res) => {  // create a get route
