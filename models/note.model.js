33f7c6b522012f052b3116f425b2e403e00fa401
joystackinfo <joynelsoninfo@gmail.com>
2025-11-06 21:00:22 +0100

    added note schema

---
+++ models/note.model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose'); // include monogoose
+const noteSchema = new mongoose.Schema({ // create note schema 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
+const note = mongoose.model('Note', userSchema);
+
+
+module.exports = Note;
--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
   next();
 });
+ 
 
 
 // Test route
